a5f04da11150d9fbb1d7cecd9c0884dca78618dc
rootkit1337tv <128513433+rootkit1337tv@users.noreply.github.com>
2024-12-18 10:27:51 +0100

    Initialized repository

---
+++ Data/Mainline/Data/HidingBarData.lua
@@ -0,0 +1,97 @@
+local NUI = unpack(NaowhUI)
+
+NUI.HidingBarData = {
+	["isDefault"] = true,
+	["config"] = {
+	["addFromDataBroker"] = true,
+	["mbtnSettings"] = {
+	},
+	["btnSettings"] = {
+	},
+	["grabMinimap"] = true,
+	["grabMinimapAfterN"] = 1,
+	["ombGrabQueue"] = {
+	},
+	["customGrabList"] = {
+	},
+	["ignoreMBtn"] = {
+	},
+	},
+	["name"] = "Naowh",
+	["bars"] = {
+	{
+	["isDefault"] = true,
+	["config"] = {
+	["lineWidth"] = 4,
+	["secondPosition"] = -130.1333534119659,
+	["hideHandler"] = 2,
+	["lineBorderColor"] = {
+	1,
+	1,
+	1,
+	1,
+	},
+	["showDelay"] = 0,
+	["bgTexture"] = "Solid",
+	["borderColor"] = {
+	1,
+	1,
+	1,
+	1,
+	},
+	["anchor"] = "top",
+	["barOffset"] = 0,
+	["barTypePosition"] = 1,
+	["size"] = 10,
+	["interceptTooltipPosition"] = 0,
+	["petBattleHide"] = true,
+	["lineColor"] = {
+	0,
+	0.6392156862745098,
+	1,
+	},
+	["position"] = 1258.666829614769,
+	["mbtnPosition"] = 2,
+	["lineBorderEdge"] = false,
+	["lineBorderOffset"] = 1,
+	["showHandler"] = 2,
+	["expand"] = 2,
+	["borderEdge"] = false,
+	["bgColor"] = {
+	0.1,
+	0.1,
+	0.1,
+	0.7,
+	},
+	["hideDelay"] = 0.75,
+	["gapSize"] = 0,
+	["interceptTooltip"] = true,
+	["buttonDirection"] = {
+	["H"] = 0,
+	["V"] = 0,
+	},
+	["borderOffset"] = 4,
+	["omb"] = {
+	["size"] = 31,
+	["distanceToBar"] = 0,
+	["lock"] = true,
+	["anchor"] = "right",
+	["barDisplacement"] = 0,
+	["canGrabbed"] = false,
+	["hide"] = true,
+	},
+	["buttonSize"] = 31,
+	["frameStrata"] = 0,
+	["lineBorderSize"] = 2,
+	["lineTexture"] = "Solid",
+	["borderSize"] = 16,
+	["fadeOpacity"] = 0.2,
+	["orientation"] = 0,
+	["rangeBetweenBtns"] = 0,
+	["fade"] = false,
+	["lock"] = true,
+	},
+	["name"] = "Bar 1",
+	},
+	},
+}
