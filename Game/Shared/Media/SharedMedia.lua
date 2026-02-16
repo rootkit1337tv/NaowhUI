998b13561599ca0bc4f08911eb855f311f9a0db8
0kamiyasha <g.greeny.8x8@gmail.com>
2026-02-16 17:03:02 -0500

    Fixed shared media to deefine sounds + prevent duplicating "Naowh" cd layouts

--- Game/Shared/Media/SharedMedia.lua
+++ Game/Shared/Media/SharedMedia.lua
@@ -2,6 +2,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
 
 local MediaType_FONT = LSM.MediaType.FONT
+local MediaType_SOUND = LSM.MediaType.SOUND
 local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
 
 LSM:Register(MediaType_FONT, "GothamNarrowUltra",       [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\GothamNarrowUltra.ttf]],                           ruRU + western)
